640337bb9ebbb9c36350a2260641234e8c3aba5c
donw385 <donw385@github.com>
2019-03-18 22:32:13 -0400

    added 2 utilities

--- lambdata_donw385/df_utils.py
+++ lambdata_donw385/df_utils.py
@@ -5,5 +5,23 @@ import pandas as pd
 
 DF_Test = pd.DataFrame([1,2,3])
 
+def delete_outliers(x):
+  elements = np.array(x)
+
+  mean = np.mean(elements, axis=0)
+  sd = np.std(elements, axis=0)
+
+  final_list = [x for x in arr if (x > mean - 2 * sd)]
+  final_list = [x for x in final_list if (x < mean + 2 * sd)]
+  print(final_list)
 
 
+def date_splitter(df):
+  df[:5] 
+  df['year'] = df['date'].dt.year 
+  df['month'] = df['date'].dt.month 
+  df['day'] = df['date'].dt.day 
+  df['hour'] = df['date'].dt.hour 
+  df['minute'] = df['date'].dt.minute
+  df_new=pd.DataFrame(df)
+  return df_new
---
+++ setup.py
@@ -0,0 +1,27 @@
+""" lambdata - a collection of Data Science helper functions
+"""
+
+import setuptools
+
+REQUIRED = [
+            "numpy",
+                "pandas"
+                ]
+
+with open("README.md", "r") as fh:
+        LONG_DESCRIPTION = fh.read()
+        
+setuptools.setup(
+  name="lambdata-donw385",
+  version="0.1.2",
+  author="donw385",
+  description="A collection of Data Science helper functions",
+  long_description=LONG_DESCRIPTION,
+  long_description_content_type="text/markdown",
+  url="https://github.com/donw385/lambdata",
+  packages=setuptools.find_packages(),
+  python_requires=">=3.5",
+  install_requires=REQUIRED,
+  classifiers=[ "Programming Language :: Python :: 3",  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent", ], )
+
